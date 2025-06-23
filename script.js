ce265537cf6037eeba3d670c93a725208d7212dc
Shashank285-001 <upshashank450@gmail.com>
2025-06-23 10:54:32 +0530

    Java sCript of clock

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let hrs=document.getElementById('hrs');
+let mins=document.getElementById('mins');
+let sec=document.getElementById('sec');
+
+setInterval(()=>{
+    let currTime=new Date();
+  hrs.innerHTML=(currTime.getHours()<10?"0":"")+ currTime.getHours();
+  mins.innerHTML=(currTime.getMinutes()<10?"0":"")+ currTime.getMinutes();
+  sec.innerHTML=(currTime.getSeconds()<10?"0":"")+ currTime.getSeconds();
+},1000)
+
